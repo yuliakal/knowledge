1b1ea2e9ec304fb94db29284cdf628016ae57f01
Yuliya Kalbanava <yuliakalbanava@gmail.com>
2018-12-05 17:57:51 +0800

    ToolsPreview component

--- components/Resource/ResourceView/Tools/ToolsPreview.tsx
+++ components/Resource/ResourceView/Tools/ToolsPreview.tsx
@@ -1,3 +1,41 @@
-const ToolsPreview = () => <div>TODO: implement ToolsPreview component</div>;
+import React from 'react';
+import { WithStyles } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
 
-export { ToolsPreview };
+import styles from './ToolsPreviewStyle';
+import Typography from "@material-ui/core/Typography/Typography";
+
+/**
+ * TOOLS PREVIEW PROPS type
+ */
+type ToolsPreviewProps = {
+  tileData: Array<string>;
+  description: string;
+};
+
+/**
+ * TOOLS PREVIEW component
+ */
+const ToolsPreview: React.SFC<ToolsPreviewProps & WithStyles> = (props) => {
+  const { classes, tileData, description } = props;
+
+  return (
+    <div className={classes.root}>
+      <GridList className={classes.gridList}>
+        {tileData.map((tile: string) => (
+          <img className={classes.image} src={tile} />
+        ))}
+      </GridList>
+      <Typography component="div" className={classes.description}>
+        {description}
+      </Typography>
+    </div>
+  );
+};
+
+// STYLEs
+const Component = withStyles(styles)(ToolsPreview);
+
+// EXPORTs
+export { Component as ToolsPreview };
